dd052cb22713c815db8f0a3b23561bc576e0400e
ggmcconomy <73233186+ggmcconomy@users.noreply.github.com>
2025-04-15 16:55:15 +0100

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-import os
-import streamlit as st
-
-csv_file = 'clean_risks.csv'
-
-st.write(f"Current working directory: {os.getcwd()}")
-st.write(f"Does the file exist? {os.path.exists(csv_file)}")
-
-df = pd.read_csv(csv_file)
