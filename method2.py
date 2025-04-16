ec25d3c06a33f870d6459e31f0a09bd57b58d318
ggmcconomy <73233186+ggmcconomy@users.noreply.github.com>
2025-04-16 14:28:08 +0100

    Update method2.py

--- method2.py
+++ method2.py
@@ -269,7 +269,7 @@ if st.session_state.access_token:
             st.session_state.token_timestamp = pd.Timestamp.now().timestamp()
 
 # --- Load Pre-Clustered Data ---
-csv_file = 'clean_risks (10).csv'
+csv_file = 'clean_risks (11).csv'
 embeddings_file = 'embeddings.npy'
 index_file = 'faiss_index.faiss'
 
